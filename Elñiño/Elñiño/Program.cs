74b2d39ff4f2d9a06c6ebe18fca8294c0007fe0a
Stuart Estevez <stuart264.se@gmail.com>
2020-07-10 19:15:04 -0400

    cambio2

--- Elñiño/Elñiño/Program.cs
+++ Elñiño/Elñiño/Program.cs
@@ -8,44 +8,86 @@ namespace Elñiño
         {
             string cedula;
             int contraseña;
-
+            string salir;
+            bool estado;
             do
             {
-                Console.Write("Ingrese su número de cédula: ");
-                cedula = Console.ReadLine();
-                Console.Write("Ingrese su contraseña: ");
-                contraseña = int.Parse(Console.ReadLine());
-                while(contraseña < 0)
+                Console.WriteLine("\nSi quieres salir de la aplicación digite una S, si quieres continuar presione *Enter.*");
+                salir = Console.ReadLine();
+                if (salir == "s" || salir == "S")
                 {
-                    Console.Write("\nNo se permiten números negativos. \n" + "Intentalo de nuevo. \n" + "\n");
-                    Console.Write("Ingrese su contraseña: ");
-                    contraseña = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\nSALISTE CON EXITO. \n");
+                    break;
                 }
-                if (cedula == "000-1234567-0" && contraseña == 2604)
-                {
-                    Console.WriteLine("\nBienvenido Stuart Estévez. *SUPERVISOR* \n");
-                }
-                else
-                {
-
-                    if (cedula == "111-1234567-1" && contraseña == 3103)
+                else while (salir == "")
                     {
-                        Console.WriteLine("\nBienvenido Steven Estévez. *ADMINISTRADOR* \n");
-                    }
-                    else
-                    {
-
-                        if (cedula == "222-1234567-2" && contraseña == 2703)
+                        Console.Write("Ingrese su número de cédula: ");
+                        cedula = Console.ReadLine();
+                        Console.Write("Ingrese su contraseña: ");
+                        contraseña = int.Parse(Console.ReadLine());
+                        while (contraseña < 0)
                         {
-                            Console.WriteLine("\nBienvenido Adrian Estévez. *VENDEDOR* \n");
+                            Console.Write("\nNo se permiten números negativos. \n" + "Intentalo de nuevo. \n" + "\n");
+                            Console.Write("Ingrese su contraseña: ");
+                            contraseña = int.Parse(Console.ReadLine());
+                        }
+                        if (cedula == "000-1234567-0" && contraseña == 2604)
+                        {
+                            estado = true;
+                            Console.WriteLine("\nBienvenido: Stuart Estévez.\n" + "\nUsername: 000-1234567-0." + "\nEstado: Activo. \n" + "Rol: Supervisor. \n" + "Fecha de creación: 26/4/2001. \n");
+                            Console.WriteLine("\nSi quieres salir pon una S");
+                            salir = Console.ReadLine();
+                            if (salir == "s" || salir == "S")
+                            {
+                                Console.WriteLine("\nSALISTE CON EXITO. \n");
+                                break;
+                            }
                         }
                         else
                         {
-                            Console.WriteLine("\nEl número de cédula y/o contraseña es incorrecta. \n" + "Intentalo de nuevo. \n");
+
+                            if (cedula == "111-1234567-1" && contraseña == 3103)
+                            {
+                                estado = false;
+                                Console.WriteLine("\nEsta cuenta está inactiva, vuelve a validar otra cuenta. \n");
+                                Console.WriteLine("\nSi quieres salir pon una S");
+                                salir = Console.ReadLine();
+                                if (salir == "s" || salir == "S")
+                                {
+                                    Console.WriteLine("\nSALISTE CON EXITO. \n");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+
+                                if (cedula == "222-1234567-2" && contraseña == 2703)
+                                {
+                                    estado = true;
+                                    Console.WriteLine("\nBienvenido: Adrian Estévez.\n" + "\nUsername: 111-1234567-1." + "\nEstado: Activo. \n" + "Rol: Vendedor. \n" + "Fecha de creación: 27/3/2001. \n");
+                                    Console.WriteLine("\nSi quieres salir pon una S");
+                                    salir = Console.ReadLine();
+                                    if (salir == "s" || salir == "S")
+                                    {
+                                        Console.WriteLine("\nSALISTE CON EXITO. \n");
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nEl número de cédula y/o contraseña es incorrecta. \n" + "Intentalo de nuevo. (Presiona *ENTER* para continuar o S para salir)");
+                                    salir = Console.ReadLine();
+                                    if (salir == "s" || salir == "S")
+                                    {
+                                        Console.WriteLine("\nSALISTE CON EXITO. \n");
+                                        break;
+                                    }
+                                }
+                            }
                         }
                     }
-                }
-            } while (contraseña == 0);
+            } while (salir == "s" || salir == "S");
+            Console.ReadKey();
 
         }
     }
